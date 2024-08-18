e73cc3bb2270797d759a57def2eab9561219b3aa
Dave Hoff <dave@blackhillsinfosec.com>
2024-08-18 13:24:17 -0500

    Add initial AQI support

--- geojson.py
+++ geojson.py
@@ -5,6 +5,123 @@ import json
 import os
 from pathlib import Path
 
+def aqi(path, name):
+    aqi_sev = {
+        "1": "Good",
+        "2": "Moderate",
+        "3": "Unhealthy for Sensitive Groups",
+        "4": "Unhealthy",
+        "5": "Very Unhealthy",
+        "6": "Hazardous"
+    }
+    aqi_styles = [
+        KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa00E53D"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly1'
+        ),
+        KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa45fafd"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly2'
+        ),
+        KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa65a4ff"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly3'
+        ),
+                KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa594cfe"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly4'
+        ),
+                KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa814db7"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly5'
+        ),
+                KML.Style(
+            KML.LineStyle(
+                KML.color("ffffff00"),
+                KML.width(.2)
+            ),
+            KML.PolyStyle(
+                KML.color("aa664d81"),
+                KML.colorMode("normal"),
+                KML.fill(1),
+                KML.outline(1)
+            ),
+            id = 'Poly6'
+        )
+    ]
+    with open(path, 'r') as f:
+        data = json.load(f)
+    parent_folder = KML.Folder(KML.name(name), KML.description("description"))
+    for feature in data['features']:
+        geom = feature['geometry']
+        status = feature['attributes']['gridcode']
+        feature_name = feature['attributes']['Id']
+        coord_str = ""
+        #for ring in geom['rings']:
+        for point in geom['rings'][0]:
+            coord_str = coord_str + '\n' + str(point[0]) + ',' + str(point[1])
+        poly = KML.Placemark(KML.name(feature_name),
+                                KML.description(aqi_sev[str(status)]),
+                                KML.styleUrl(f"#Poly{status}"),
+                                KML.Polygon(KML.extrude('1'),
+                                            KML.altitudeMode('clampToGround'),
+                                            KML.outerBoundaryIs(KML.LinearRing(KML.coordinates(coord_str)))))
+        parent_folder.append(poly)
+    doc = KML.kml(KML.Document(
+        KML.name(name)
+    ))
+    for kmlstyle in aqi_styles:
+        doc.Document.append(kmlstyle)
+    for x in parent_folder.findall('.//{http://www.opengis.net/kml/2.2}Placemark'):
+        doc.Document.append(x)
+    return(doc)
+
+
 def geojson_to_kml(path, name):
     with open(path, 'r') as f:
         data = json.load(f)
@@ -17,17 +134,27 @@ def geojson_to_kml(path, name):
             coord_str = ""
             for point in geom["coordinates"][0]:
                 coord_str = coord_str + '\n' + str(point[0]) + ',' + str(point[1])
-            poly = KML.Placemark(KML.name(feature["properties"]["NAME"]),
+            try:
+                feature_name = feature["properties"]["NAME"]
+            except KeyError:
+                feature_name = feature["properties"]["Id"]
+
+            try:
+                poly = KML.Placemark(KML.name(feature_name),
                                  KML.description(feature['properties']['STATEFP']),
                                 KML.styleUrl("#Poly1"),
                                 KML.Polygon(KML.extrude('1'),
                                             KML.altitudeMode('clampToGround'),
                                             KML.outerBoundaryIs(KML.LinearRing(KML.coordinates(coord_str)))
                                             ))
-            #poly = KML.Placemark(KML.name(feature["properties"]["NAME"]), 
-            #                     KML.LineString(KML.extrude('1'),
-            #                                KML.altitudeMode('clampToGround'),
-            #                                KML.coordinates(coord_str)))
+            except KeyError:
+                poly = KML.Placemark(KML.name(feature_name),
+                                    KML.styleUrl("#Poly1"),
+                                    KML.Polygon(KML.extrude('1'),
+                                                KML.altitudeMode('clampToGround'),
+                                                KML.outerBoundaryIs(KML.LinearRing(KML.coordinates(coord_str)))
+                                    ))
+
             parent_folder.append(poly)
         #elif geom_type == 'MultiPolygon':
         #    #print(geom["coordinates"])
@@ -49,9 +176,10 @@ def geojson_to_kml(path, name):
                                             KML.altitudeMode('relativeToGround'),
                                             KML.coordinates(geom["coordinates"][0])))
         elif geom_type == 'Point':
-            KML.Placemark(KML.name(feature["properties"["NAME"]]),
+            KML.Placemark(KML.name(feature["properties"]["NAME"]),
                         KML.Point(
-                        KML.coordinates(feature["coordinates"][0])))
+                        #KML.coordinates(feature["coordinates"][0])))
+                        KML.coordinates(geom["coordinates"][0])))
         else:
             print("ERROR: unknown type:", geom_type)
     #write_kml(parent_folder)
--- utils.py
+++ utils.py
@@ -27,15 +27,19 @@ def progress_bar(msg, current, total):
 
 def dl_with_retry(url, max_tries=3):
     success = False
-    tries = 0
+    tries = 1
     # TODO: Create get_with_retry method in utils
     while not success:
         r = requests.get(url, verify=False)
-        if (r.status_code == 200 or tries == 2):
+        if (r.status_code == 200):
             success = True
             return r.content
+        elif tries == max_tries:
+            print("File could not be downloaded after {max_tries} attempts")
+            raise RuntimeError
         else:
             tries = tries + 1
+    return r.content
 
 
 class JSONWithCommentsDecoder(json.JSONDecoder):
